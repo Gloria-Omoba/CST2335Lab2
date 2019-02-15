19db240dcd71bbb4cbece233a5895e092b1fe08b
0moba Gloria <Omob0001@algonquinlive.com>
2019-02-14 19:28:55 -0500

    lab 3

--- app/src/main/java/com/example/lab2/MainActivity.java
+++ app/src/main/java/com/example/lab2/MainActivity.java
@@ -1,16 +1,55 @@
 package com.example.lab2;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    SharedPreferences email;
+    EditText typeField;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_main);
-        setContentView(R.layout.activity_main_grid);
+        //setContentView(R.layout.activity_main_grid);
         //setContentView(R.layout.activity_main_relative);
         //setContentView(R.layout.activity_main_linear);
+        setContentView(R.layout.activity_main_lab3);
+        //setContentView(R.layout.activity_main_lab3b);
+
+        typeField =(EditText)findViewById(R.id.editTextView2);
+        email= getSharedPreferences("FileName",Context.MODE_PRIVATE);
+        String savedEmail= email.getString("Email",  " ");
+
+        typeField.setText(savedEmail);
+
+        //Setting onClickListener for login button
+        Button loginButton = (Button)findViewById(R.id.button3);
+        loginButton.setOnClickListener( b -> {
+
+            //Give directions to go from this page, to SecondActivity
+            Intent nextPage = new Intent(MainActivity.this,ProfileActivity.class);
+
+            //Now make the transition:
+            startActivityForResult(nextPage, 2);
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        SharedPreferences.Editor editor = email.edit();
+
+        //to save what was typed
+        String inputTyped = typeField.getText().toString();
+
+        editor.putString("Email",inputTyped);
+
+        editor.commit();
     }
 }
---
+++ app/src/main/java/com/example/lab2/ProfileActivity.java
@@ -0,0 +1,72 @@
+package com.example.lab2;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageButton;
+
+
+public class ProfileActivity extends AppCompatActivity {
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    ImageButton mImageButton;
+    public static final String ACTIVITY_NAME = "PROFILE_ACTIVITY";
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_lab3b);
+
+        mImageButton = (ImageButton)findViewById(R.id.imageButton2);
+
+        Log.e(ACTIVITY_NAME,"OnCreate" );
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.e(ACTIVITY_NAME,"OnStart" );
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e(ACTIVITY_NAME,"OnResume" );
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.e(ACTIVITY_NAME,"OnPause" );
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.e(ACTIVITY_NAME,"OnStop" );
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e(ACTIVITY_NAME,"onDestroy" );
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            mImageButton.setImageBitmap(imageBitmap);
+            Log.e(ACTIVITY_NAME,"onActivityResult" );
+        }
+    }
+
+    private void dispatchTakePictureIntent () {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+}
