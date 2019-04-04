1765eff9b289a7fa2aa0b49484fd7399bbf2c0a0
0moba Gloria <Omob0001@algonquinlive.com>
2019-04-04 19:12:55 -0400

    final lab 6

--- app/src/main/java/com/example/lab2/TestToolbar.java
+++ app/src/main/java/com/example/lab2/TestToolbar.java
@@ -1,5 +1,6 @@
 package com.example.lab2;
 
+import android.app.Activity;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
@@ -15,7 +16,7 @@ import android.widget.Toast;
 
 public class TestToolbar extends AppCompatActivity {
 
-    String overflowMessage = "You clicked on the overflow menu";
+    String initial = "This is the initial message";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,27 +24,27 @@ public class TestToolbar extends AppCompatActivity {
         setContentView(R.layout.activity_test_toolbar);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
     }
 
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
 
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.main_menu, menu);
-        return true;
+            MenuInflater inflater = getMenuInflater();
+            inflater.inflate(R.menu.main_menu, menu);
+            return true;
     }
 
     public boolean onOptionsItemSelected (MenuItem item){
         switch(item.getItemId())
         {
 
-            case R.id.action_star:
-                Toast.makeText(this, "This is the initial message", Toast.LENGTH_LONG).show();
+            case R.id.action_choice1:
+                Toast.makeText(this, initial, Toast.LENGTH_LONG).show();
                 return true;
-            case R.id.action_point:
-                alertExample();
+            case R.id.action_choice2:
+                dialogBox();
                 return true;
-            case R.id.action_lock:
+            case R.id.action_choice3:
                 Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
                 setSupportActionBar(toolbar);
                 Snackbar snackbar = Snackbar.make(toolbar, "Clicked item 3", Snackbar.LENGTH_LONG)
@@ -51,23 +52,22 @@ public class TestToolbar extends AppCompatActivity {
                 snackbar.show();
                 return true;
             case R.id.overflow_menu:
-                Toast.makeText(this,overflowMessage, Toast.LENGTH_LONG).show();
+                Toast.makeText(this,"You clicked on the overflow menu", Toast.LENGTH_LONG).show();
                 return true;
         }
         return true;
     }
 
-    public void alertExample()
-    {
+    public void dialogBox() {
         View dialogBox = getLayoutInflater().inflate(R.layout.activity_dialog, null);
         EditText editText = (EditText)dialogBox.findViewById(R.id.editText);
 
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage("Change overflow message ")
+        AlertDialog.Builder builder = new AlertDialog.Builder(TestToolbar.this);
+        builder.setMessage("dialog box message ")
                 .setPositiveButton("Positive", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
 
-                        overflowMessage = editText.getText().toString();
+                        initial = editText.getText().toString();
                     }
                 })
                 .setNegativeButton("Negative", new DialogInterface.OnClickListener() {
@@ -79,3 +79,4 @@ public class TestToolbar extends AppCompatActivity {
         builder.create().show();
     }
 }
+
